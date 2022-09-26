831aee4f5b9d7c666b2b79fe275da43d6bd6490d
Skul1CrowN <boat.72545@gmail.com>
2022-09-26 15:17:02 +0700

    Add schedule task for bot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 const fs = require('fs');
+const cron = require('node-cron')
+require('dotenv').config();
 
 const url = "https://www.goodsmile.info/en/products/announced/";
 
-let products = []
+const sendTelegramMessage = async (message) => {
+	try {
+		await axios(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			data: {
+				chat_id: process.env.CHAT_ID,
+				text: message,
+				disable_notification: true,
+			},
+		});
+	} catch (error) {
+		console.log(`Can't send telegram message!`, error);
+	}
+};
 
-const scrape = async () => {
+const scrapeData = async () => {
+  let products = []
     try {
       const { data } = await axios.get(url);
       const $ = cheerio.load(data);
@@ -24,12 +43,44 @@ const scrape = async () => {
           product.name = $(elem).text().trim().replace('\n\t\t\t\t\t\t\n                ',' ')
           products.push(product)
         }
-      })
-      fs.writeFileSync('test5.json', JSON.stringify(products))
-      // fs.writeFileSync('test3.json', JSON.stringify(products))
-      // fs.writeFileSync('test4.txt', products)
+      });
     } catch(err) {
-      console.log(err)
+      console.log(err);
     }
+    return products;
 }
-scrape()
+
+cron.schedule('* * * * *', async () => {
+  console.log('Running task every minute');
+  let currentData = [];
+  try {
+    currentData = fs.readFileSync('./src/data/products.json', 'utf-8');
+    currentData = JSON.parse(currentData);
+  } catch(err) {
+    currentData = [];
+  }
+
+  const newData = await scrapeData();
+  fs.writeFileSync('./src/test/test6.json', JSON.stringify(newData)); 
+  newData.forEach(async(e) => {
+    let New = true;
+    for(let i=0;i<currentData.length;i++) {
+      if(currentData[i].name === e.name) {
+        New = false;
+        break;
+      }
+    }
+    if (New) {
+  const msg = `=================================================
+New Product Announced 📢
+Date: ${e.date}
+Name: ${e.name}
+Link: ${e.link}
+=================================================`;
+
+    await sendTelegramMessage(msg);
+    }
+  });
+
+  fs.writeFileSync('.src/data/products.json', JSON.stringify(newData));
+});
